aba30bc11e3ba278686c26094485c60f894131cd
REXIANN <gandolkim@gmail.com>
2021-10-22 00:38:46 +0900

    init

---
+++ 2-types/1.ts
@@ -0,0 +1,18 @@
+export {};
+
+const size: number = 123;
+const isBig: boolean = size >= 100;
+const msg: string = isBig ? '크다' : '작다';
+
+const values: number[] = [1, 2, 3];
+const values2: Array<number> = [1, 2, 3];
+values.push('a');
+
+const data: [string, number] = [msg, size]; // tuple
+data[0].substr(1);
+data[1].substr(1);
+
+console.log('type of :  123', typeof 123)
+console.log('type of : "abc"', typeof 'abc')
+console.log('type of : [1, 2, 3]', typeof [1, 2, 3]) // object
+// JS에 Array 타입은 없고 object 타입만 있음
